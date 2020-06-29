1ee6032dca551ba4cfec26995085b7662fcf552f
BodomLake <7922944021@qq.com>
2020-06-29 11:55:27 +0800

    同花顺股票财务数据获取scrap

---
+++ log/logger.py
@@ -0,0 +1,14 @@
+import logging
+from logging import handlers
+
+
+class Logger(object):
+    # 日志级别关系映射
+    level_relations = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'crit': logging.CRITICAL
+    }
+
