b05212d4b8406f16b35d8e79370ea3c580af7358
BodomLake <7922944021@qq.com>
2020-06-29 11:51:04 +0800

    同花顺股票财务数据获取scrap

--- config/config.py
+++ config/config.py
@@ -1,6 +1,9 @@
 # '数据库类型+数据库驱动名称://用户名:口令@机器地址:端口号/数据库名'
 def dbConnInfo(user='root', password='rex123456', host='127.0.0.1', port='3306', args='charset=utf8mb4', schema='stock'):
-    return 'mysql+pymysql://' + user + ':' + password + '@' + host + ':' + port + '/' + schema + '?' + args
+    # pymysql 会 报出警告
+    # return 'mysql+pymysql://' + user + ':' + password + '@' + host + ':' + port + '/' + schema + '?' + args
+    return 'mysql+mysqlconnector://' + user + ':' + password + '@' + host + ':' + port + '/' + schema + '?' + args
+
 
 driverpath = r'D:\Python37\Lib\site-packages\selenium\webdriver\chrome\chromedriver83.0.4103.exe'
 
